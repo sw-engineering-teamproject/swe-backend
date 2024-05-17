5104d94931bb322b4ecc8a898df61a8156387c60
hong-sile <hyukjoon.dev@gmail.com>
2024-05-17 15:47:02 +0900

    feat: 프로젝트 생성 기능 추가(#15)

---
+++ core/src/main/java/swe/project/domain/ProjectRepository.java
@@ -0,0 +1,13 @@
+package swe.project.domain;
+
+import static swe.project.exception.ProjectExceptionType.NOT_FOUND_PROJECT;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import swe.project.exception.ProjectException;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+  default Project readById(final Long id) {
+    return findById(id).orElseThrow(() -> new ProjectException(NOT_FOUND_PROJECT));
+  }
+}
