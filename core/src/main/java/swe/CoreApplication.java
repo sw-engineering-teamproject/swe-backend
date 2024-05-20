fd8605a70904345f96abc83642a3c7a409737211
hong-sile <hyukjoon.dev@gmail.com>
2024-05-20 16:46:36 +0900

    chore: application yml 제대로 부를 수 있도록 수정(#31)

--- core/src/main/java/swe/CoreApplication.java
+++ core/src/main/java/swe/CoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreApplication {
 
   public static void main(String[] args) {
+    System.setProperty("spring.config.name", "application-core");
     SpringApplication.run(CoreApplication.class, args);
   }
 
