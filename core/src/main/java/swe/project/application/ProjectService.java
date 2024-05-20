1dac9e84518995aa5ed8371364c270539b9a323a
hong-sile <hyukjoon.dev@gmail.com>
2024-05-20 12:31:00 +0900

    refactor: creator -> reporter로 변경(#20)

--- core/src/main/java/swe/project/application/ProjectService.java
+++ core/src/main/java/swe/project/application/ProjectService.java
@@ -1,10 +1,12 @@
 package swe.project.application;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import swe.project.domain.Project;
 import swe.project.domain.ProjectRepository;
+import swe.project.dto.ProjectListResponse;
 import swe.user.domain.User;
 import swe.user.domain.UserRepository;
 
@@ -19,10 +21,14 @@ public class ProjectService {
   public Long createProject(final String title, final Long creatorId) {
     final User creator = userRepository.readById(creatorId);
     final Project project = Project.builder()
-        .creatorId(creator.getId())
+        .reporterId(creator.getId())
         .title(title)
         .build();
 
     return projectRepository.save(project).getId();
+  }
+
+  public List<ProjectListResponse> readAllProject() {
+    return null;
   }
 }
