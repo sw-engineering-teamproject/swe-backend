91f4f732f2e39afc0825a98673fa6006004968cd
hong-sile <hyukjoon.dev@gmail.com>
2024-05-21 16:33:03 +0900

    feat: core의 application-core.yml 참조하도록 수정(#8)

--- gui/src/main/java/swe/GuiApplication.java
+++ gui/src/main/java/swe/GuiApplication.java
@@ -1,20 +1,17 @@
 package swe;
 
-import javax.swing.JFrame;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import swe.gui.issue.IssuePage;
-import swe.gui.project.ProjectPage;
 import swe.gui.user.AuthPage;
-import swe.user.application.UserService;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class GuiApplication {
 
   public static void main(String[] args) {
+    System.setProperty("spring.config.name", "application-core");
     ApplicationContext applicationContext = SpringApplication.run(GuiApplication.class, args);
     System.setProperty("java.awt.headless", "false");
     new AuthPage(applicationContext);
