d77cc8e693e875e4a65336422f2459af4674dbd1
hong-sile <hyukjoon.dev@gmail.com>
2024-05-26 16:30:14 +0900

    feat: 이슈 상태를 변경하는 기능 추가(#41)

--- web/src/main/java/swe/controller/IssueController.java
+++ web/src/main/java/swe/controller/IssueController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import swe.dto.CommentAddRequest;
 import swe.dto.IssueDescriptionUpdateRequest;
 import swe.dto.IssueResponse;
+import swe.dto.IssueStatusUpdateRequest;
 import swe.issue.application.IssueService;
 import swe.issue.dto.IssueCreateRequest;
 import swe.user.dto.JwtMemberId;
@@ -66,6 +67,15 @@ public class IssueController {
       @RequestBody final IssueDescriptionUpdateRequest issueDescriptionUpdateRequest
   ) {
     issueService.updateDescription(issueId, issueDescriptionUpdateRequest.newDescription());
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/issues/{issueId}/status")
+  public ResponseEntity<Void> updateStatus(
+      @PathVariable final Long issueId,
+      @RequestBody final IssueStatusUpdateRequest issueStatusUpdateRequest
+  ) {
+    issueService.updateStatus(issueId, issueStatusUpdateRequest.statusName());
     return ResponseEntity.ok().build();
   }
 }
