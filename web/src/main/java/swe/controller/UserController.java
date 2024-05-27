81585b26f3482fb455a9fe7d6a4acfc337b4fa5e
hong-sile <hyukjoon.dev@gmail.com>
2024-05-27 09:56:09 +0900

    feat: 유저 목록을 반환하는 APi 추가(#54)

--- web/src/main/java/swe/controller/UserController.java
+++ web/src/main/java/swe/controller/UserController.java
@@ -1,5 +1,6 @@
 package swe.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import swe.dto.user.DuplicateNicknameRequest;
 import swe.dto.user.DuplicateNicknameResponse;
 import swe.dto.user.LoginResponse;
 import swe.dto.user.UserLoginRequest;
+import swe.dto.user.UserResponse;
 import swe.dto.user.UserRoleResponse;
 import swe.user.application.JwtProvider;
 import swe.user.application.UserService;
@@ -33,6 +35,12 @@ public class UserController {
     var user = userService.login(request.accountId(), request.password());
     final String accessToken = jwtProvider.createAccessTokenWith(user.getId());
     return ResponseEntity.ok(new LoginResponse(accessToken));
+  }
+
+  @GetMapping("/users")
+  public ResponseEntity<List<UserResponse>> userList() {
+    final var users = userService.findAllUsers();
+    return ResponseEntity.ok(UserResponse.createList(users));
   }
 
   @PostMapping("/users/nickname/check")
